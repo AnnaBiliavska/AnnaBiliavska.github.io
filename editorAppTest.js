ccbe9c8463d7847e9be9417cd46b4ab407ca8495
annab <annab@wix.com>
2018-02-07 12:22:29 +0200

    change

--- editorAppTest.js
+++ editorAppTest.js
@@ -24,7 +24,7 @@ function getAppManifest() {
                                 }
                             },
                             behavior: {
-                                "rotatable": false,
+                                rotatable: 'false',
                             },
                         }
                     },
