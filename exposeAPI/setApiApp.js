d4e22baf19ef4be2a28003fa9fd4142d43ae8fdd
annab <annab@wix.com>
2018-02-18 14:50:03 +0200

    add apps + delete widget

--- exposeAPI/setApiApp.js
+++ exposeAPI/setApiApp.js
@@ -2,3 +2,25 @@
 
 let editorSDK;
 
+async function updateButtonLabel(buttonRef, label) {
+    return await editorSDK.document.components.data.update('', {
+        componentRef: buttonRef,
+        data: {label}
+    });
+}
+
+
+module.exports = {
+    onEvent: () => {},
+    getAppManifest: () => ({}),
+    editorReady: async function editorReady(_editorSDK, appDefinitionId) {
+        const loadedButton = {
+            'type': 'DESKTOP',
+            'id': 'comp-jcueepln'
+        }
+        editorSDK = _editorSDK;
+        await editorSDK.editor.setAppAPI(appDefinitionId, {updateButtonLabel});
+        return await updateButtonLabel(loadedButton, 'Loaded');
+    },
+    exports: {updateButtonLabel}
+};
