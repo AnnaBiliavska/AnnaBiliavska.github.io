aa745b76be694f87365e53625f8582bce700e855
annab <annab@wix.com>
2018-02-19 16:49:23 +0200

    create getApiApp

---
+++ exposeAPI/getApiApp.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    editorReady: async function (editorSDK, appDefinitionId) {
+        const appPublicAPI = await editorSDK.document.application.getPublicAPI(appDefinitionId, {appDefinitionId: '151294ae-dbdf-1943-4d8c-d6bdadf56ec2'});
+        await appPublicAPI.sendLog();
+    },
+    getAppManifest: () => ({}),
+    onEvent: () => {}
+};
