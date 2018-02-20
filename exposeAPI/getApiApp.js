a2ad1c0c46cf97cb29f5ec6d68761c5be778247d
Omer Ganim <ganimomer@gmail.com>
2018-02-20 18:02:25 +0200

    Wrap getAppApi with IIFE

--- exposeAPI/getApiApp.js
+++ exposeAPI/getApiApp.js
@@ -1,10 +1,11 @@
 'use strict';
-
-module.exports = {
-    editorReady: async function (editorSDK, appDefinitionId) {
-        const appPublicAPI = await editorSDK.document.application.getPublicAPI(appDefinitionId, {appDefinitionId: '151294ae-dbdf-1943-4d8c-d6bdadf56ec2'});
-        await appPublicAPI.sendLog("Got it");
-    },
-    getAppManifest: () => ({}),
-    onEvent: () => {}
-};
+(function() {
+    module.exports = {
+        editorReady: async function (editorSDK, appDefinitionId) {
+            const appPublicAPI = await editorSDK.document.application.getPublicAPI(appDefinitionId, {appDefinitionId: '151294ae-dbdf-1943-4d8c-d6bdadf56ec2'});
+            await appPublicAPI.sendLog("Got it");
+        },
+        getAppManifest: () => ({}),
+        onEvent: () => {}
+    };
+}());
