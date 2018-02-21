23d843a360a8aedc04c456df2450fbe76dbf179a
annab <annab@wix.com>
2018-02-21 14:27:58 +0200

    wrap

--- exposeAPI/setApiApp.js
+++ exposeAPI/setApiApp.js
@@ -1,21 +1,23 @@
 'use strict';
 
-
-let editorSDK;
+(function() {
+    let editorSDK;
 
     async function sendLog(text) {
         return await console.log(text);
     }
 
     module.exports = {
-            onEvent: () => {},
-            getAppManifest: () => ({}),
-            editorReady: async function editorReady(_editorSDK, appDefinitionId) {
-                editorSDK = _editorSDK;
-                await editorSDK.editor.setAppAPI(appDefinitionId, {sendLog});
-                return await sendLog("Set API");
-                return await sendLog("Set APIzzz");
-            },
-            exports: {sendLog}
+        onEvent: () => {
+        },
+        getAppManifest: () => ({}),
+        editorReady: async function editorReady(_editorSDK, appDefinitionId) {
+            editorSDK = _editorSDK;
+            await editorSDK.editor.setAppAPI(appDefinitionId, {sendLog});
+            return await sendLog("Set API");
+            return await sendLog("Set APIzzz");
+        },
+        exports: {sendLog}
     };
+}());
 
