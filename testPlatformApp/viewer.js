bb6873c5a2376fb33d75397941e328f6b4d6ae8a
annab <annab@wix.com>
2018-03-18 19:57:18 +0200

    add viewer js + expose api for editorSdk app

--- testPlatformApp/viewer.js
+++ testPlatformApp/viewer.js
@@ -1,7 +1,45 @@
+let openPopUp;
+const subscribers = [];
+
 function initAppForPage() {
     console.log("initAppForPage");
 }
 
+var position =  {origin: Wix.WindowOrigin.FIXED, placement: Wix.WindowPlacement.CENTER};
+var onClose = function(message) { console.log("popup closed", message) };
+
+function openPopUp(url) {
+    Wix.openPopup(url, 400, 400, position, onClose);
+}
+
+
+function createControllers(controllerConfigs) {
+    console.log("controllerConfigs", controllerConfigs);
+    return controllerConfigs.map(controllerConfig => {
+        const compId = controllerConfig.config.compId;
+        console.log("compId", compId);
+        return {
+            exports: {
+                openPopUp: function (url) {
+                    if (_.isFunction(openPopUp)) {
+                        openPopUp(url);
+                    } else {
+                        console.log("Error");
+                    }
+                }
+            },
+            pageReady: _.noop
+        }
+    });
+}
+
+
 module.exports = {
     initAppForPage,
+    createControllers,
+    exports: {
+        registerOpenPopUpFunction: function (func) {
+            openPopUp = func
+        }
+    }
 };
